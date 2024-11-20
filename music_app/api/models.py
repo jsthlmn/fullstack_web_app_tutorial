509276e97eb3b6b73dbdde4a54c90bde6abbf177
hilman <hpanser@gmail.com>
2024-11-20 20:34:46 +0700

    update

--- music_app/api/models.py
+++ music_app/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8, default='', unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guess_can_pause = models.BooleanField(null=False, default=False)
